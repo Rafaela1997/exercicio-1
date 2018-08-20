7697fe294b50b26441d2c5a28268e8a7e3ea74d1
klngrrr <negraoklara@gmail.com>
2018-08-20 12:23:41 -0300

    meu exercicio de js 1

---
+++ script.js
@@ -0,0 +1,19 @@
+let idade = prompt("digite sua idade");
+
+let renda = prompt("digite sua renda");
+
+let valorDoEmprestimo = prompt("digite o valor do empretimo");
+
+
+
+
+
+if (idade >= 21 && idade <= 60 && renda >= 1500 ){ 
+alert("aprovado");
+let NumeroDeParcelas = prompt("digite o numero de parcelas");
+
+}
+ 
+else{
+    alert("não aprovado")
+}
